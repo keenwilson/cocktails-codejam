308c742e08d582f85387579f764b2e63c038267e
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-10 16:52:43 -0600

    Initial set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    CocktailsListComponent
   ],
   imports: [
     BrowserModule
