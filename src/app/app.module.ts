781ab106054bb274baf9a04ce790a228fe71cec4
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-10 16:57:09 -0600

    Add header and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailsComponent,
-    CocktailsListComponent
+    CocktailsListComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
