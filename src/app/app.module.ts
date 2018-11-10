b837b5847c9c3922101eb5264103abef8ede4ef9
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-10 17:46:51 -0600

    First draft cocktails list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
@@ -18,7 +19,8 @@ import { CocktailComponent } from './cocktail/cocktail.component';
     CocktailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
