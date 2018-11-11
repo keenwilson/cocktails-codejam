8bbdcef44c8535f73d3a5f53b188bfa121927190
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-10 20:36:40 -0600

    Add pink filter to profile pictures

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
- 
-import { DashboardComponent }   from './dashboard/dashboard.component';
 import { CocktailsListComponent }      from './cocktails-list/cocktails-list.component';
-import { DetailsComponent }  from './details/details.component';
+
  
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: DetailsComponent },
   { path: 'cocktails', component: CocktailsListComponent }
 ];
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { DetailsComponent } from './details/details.component';
 import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
-import { CocktailComponent } from './cocktail/cocktail.component';
-import { MessageComponent } from './message/message.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent,
     CocktailsListComponent,
     HeaderComponent,
-    AboutComponent,
-    CocktailComponent,
-    MessageComponent
+    AboutComponent
   ],
   imports: [
     BrowserModule,
