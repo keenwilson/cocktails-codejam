adfd0d6c1f48ae364d8a92f43c60a66fcb0e85a8
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-11-10 20:58:40 -0600

    Clear unused folders

--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -4,18 +4,15 @@ import { Observable, of } from 'rxjs';
  
 import { Cocktail } from './cocktail';
 import { COCKTAILS } from './mock-cocktails';
-import { MessageService } from './message.service';
+
  
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailService {
  
-  constructor(private messageService: MessageService) { }
+  constructor() { }
  
-  getCocktails(): Observable<Cocktail[]> {
-    // TODO: send the message _after_ fetching the cocktails
-    this.messageService.add('CocktailService: fetched cocktails');
-    return of(COCKTAILS);
-  }
+  
+  
 }
